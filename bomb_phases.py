d8747da15af8152bd9011980a0e469631e624f9e
lucasprocious <lucasprocious@gmail.com>
2025-11-19 15:21:29 -0500

    Update bomb_phases.py

--- bomb_phases.py
+++ bomb_phases.py
@@ -46,28 +46,42 @@ class Lcd(Frame):
         # the timer
         self._ltimer = Label(self, bg="black", fg="#00ff00", font=("Courier New", 18), text="Time left: ")
         self._ltimer.grid(row=1, column=0, columnspan=3, sticky=W)
+        # the math challenge status
+        self._lmath = Label(self, bg="black", fg="#ffff00", font=("Courier New", 18), text="Math Challenge: ", justify=LEFT)
+        self._lmath.grid(row=2, column=0, columnspan=3, sticky=W)
         # the keypad passphrase
-        self._lkeypad = Label(self, bg="black", fg="#00ff00", font=("Courier New", 18), text="Keypad phase: ")
-        self._lkeypad.grid(row=2, column=0, columnspan=3, sticky=W)
+        self._lkeypad = Label(self, bg="black", fg="#666666", font=("Courier New", 18), text="Keypad phase: LOCKED")
+        self._lkeypad.grid(row=3, column=0, columnspan=3, sticky=W)
         # the jumper wires status
-        self._lwires = Label(self, bg="black", fg="#00ff00", font=("Courier New", 18), text="Wires phase: ")
-        self._lwires.grid(row=3, column=0, columnspan=3, sticky=W)
+        self._lwires = Label(self, bg="black", fg="#666666", font=("Courier New", 18), text="Wires phase: LOCKED")
+        self._lwires.grid(row=4, column=0, columnspan=3, sticky=W)
         # the pushbutton status
-        self._lbutton = Label(self, bg="black", fg="#00ff00", font=("Courier New", 18), text="Button phase: ")
-        self._lbutton.grid(row=4, column=0, columnspan=3, sticky=W)
+        self._lbutton = Label(self, bg="black", fg="#666666", font=("Courier New", 18), text="Button phase: LOCKED")
+        self._lbutton.grid(row=5, column=0, columnspan=3, sticky=W)
         # the toggle switches status
-        self._ltoggles = Label(self, bg="black", fg="#00ff00", font=("Courier New", 18), text="Toggles phase: ")
-        self._ltoggles.grid(row=5, column=0, columnspan=2, sticky=W)
+        self._ltoggles = Label(self, bg="black", fg="#666666", font=("Courier New", 18), text="Toggles phase: LOCKED")
+        self._ltoggles.grid(row=6, column=0, columnspan=2, sticky=W)
         # the strikes left
         self._lstrikes = Label(self, bg="black", fg="#00ff00", font=("Courier New", 18), text="Strikes left: ")
-        self._lstrikes.grid(row=5, column=2, sticky=W)
+        self._lstrikes.grid(row=6, column=2, sticky=W)
         if (SHOW_BUTTONS):
             # the pause button (pauses the timer)
             self._bpause = tkinter.Button(self, bg="red", fg="white", font=("Courier New", 18), text="Pause", anchor=CENTER, command=self.pause)
-            self._bpause.grid(row=6, column=0, pady=40)
+            self._bpause.grid(row=7, column=0, pady=40)
             # the quit button
             self._bquit = tkinter.Button(self, bg="red", fg="white", font=("Courier New", 18), text="Quit", anchor=CENTER, command=self.quit)
-            self._bquit.grid(row=6, column=2, pady=40)
+            self._bquit.grid(row=7, column=2, pady=40)
+
+    # unlock other phases on the GUI
+    def unlockPhases(self):
+        self._lkeypad["fg"] = "#00ff00"
+        self._lkeypad["text"] = "Keypad phase: "
+        self._lwires["fg"] = "#00ff00"
+        self._lwires["text"] = "Wires phase: "
+        self._lbutton["fg"] = "#00ff00"
+        self._lbutton["text"] = "Button phase: "
+        self._ltoggles["fg"] = "#00ff00"
+        self._ltoggles["text"] = "Toggles phase: "
 
     # lets us pause/unpause the timer (7-segment display)
     def setTimer(self, timer):
@@ -87,6 +101,7 @@ class Lcd(Frame):
         # destroy/clear widgets that are no longer needed
         self._lscroll["text"] = ""
         self._ltimer.destroy()
+        self._lmath.destroy()
         self._lkeypad.destroy()
         self._lwires.destroy()
         self._lbutton.destroy()
@@ -187,12 +202,18 @@ class Timer(PhaseThread):
     def __str__(self):
         return f"{self._min}:{self._sec}"
 
-# the keypad phase
-class Keypad(PhaseThread):
-    def __init__(self, component, target, name="Keypad"):
-        super().__init__(name, component, target)
-        # the default value is an empty string
+# the math challenge phase (must be completed before other phases unlock)
+class MathChallenge(PhaseThread):
+    def __init__(self, component, questions, name="MathChallenge"):
+        super().__init__(name, component)
+        # the math questions to solve
+        self._questions = questions
+        # current question index
+        self._current_question = 0
+        # the current answer being entered
         self._value = ""
+        # track if all questions are answered
+        self._all_complete = False
 
     # runs the thread
     def run(self):
@@ -208,19 +229,86 @@ class Keypad(PhaseThread):
                     except:
                         key = ""
                     sleep(0.1)
-                # log the key
-                self._value += str(key)
-                # the combination is correct -> phase defused
-                if (self._value == self._target):
-                    self._defused = True
-                # the combination is incorrect -> phase failed (strike)
-                elif (self._value != self._target[0:len(self._value)]):
-                    self._failed = True
+                
+                # handle special keys
+                if (key == "*"):
+                    # clear the current input
+                    self._value = ""
+                elif (key == "#"):
+                    # submit the answer
+                    if (self._value):
+                        correct_answer = self._questions[self._current_question]['answer']
+                        if (self._value == correct_answer):
+                            # correct answer!
+                            self._current_question += 1
+                            self._value = ""
+                            # check if all questions are answered
+                            if (self._current_question >= len(self._questions)):
+                                self._defused = True
+                                self._all_complete = True
+                                self._running = False
+                        else:
+                            # wrong answer -> strike
+                            self._failed = True
+                            self._value = ""
+                else:
+                    # add the digit to the current answer
+                    self._value += str(key)
+            
+            sleep(0.1)
+
+    # returns the current question and answer progress as a string
+    def __str__(self):
+        if (self._all_complete):
+            return "ALL QUESTIONS SOLVED - PHASES UNLOCKED!"
+        else:
+            question_text = self._questions[self._current_question]['question']
+            return f"Q{self._current_question + 1}/{len(self._questions)}: {question_text} = {self._value}_"
+
+# the keypad phase (now only active after math challenge)
+class Keypad(PhaseThread):
+    def __init__(self, component, target, name="Keypad"):
+        super().__init__(name, component, target)
+        # the default value is an empty string
+        self._value = ""
+        # is this phase locked?
+        self._locked = True
+
+    # unlocks the phase
+    def unlock(self):
+        self._locked = False
+
+    # runs the thread
+    def run(self):
+        self._running = True
+        while (self._running):
+            # only process if unlocked
+            if (not self._locked):
+                # process keys when keypad key(s) are pressed
+                if (self._component.pressed_keys):
+                    # debounce
+                    while (self._component.pressed_keys):
+                        try:
+                            # just grab the first key pressed if more than one were pressed
+                            key = self._component.pressed_keys[0]
+                        except:
+                            key = ""
+                        sleep(0.1)
+                    # log the key
+                    self._value += str(key)
+                    # the combination is correct -> phase defused
+                    if (self._value == self._target):
+                        self._defused = True
+                    # the combination is incorrect -> phase failed (strike)
+                    elif (self._value != self._target[0:len(self._value)]):
+                        self._failed = True
             sleep(0.1)
 
     # returns the keypad combination as a string
     def __str__(self):
-        if (self._defused):
+        if (self._locked):
+            return "LOCKED"
+        elif (self._defused):
             return "DEFUSED"
         else:
             return self._value
@@ -229,6 +317,10 @@ class Keypad(PhaseThread):
 class Wires(PhaseThread):
     def __init__(self, component, target, name="Wires"):
         super().__init__(name, component, target)
+        self._locked = True
+
+    def unlock(self):
+        self._locked = False
 
     # runs the thread
     def run(self):
@@ -237,11 +329,13 @@ class Wires(PhaseThread):
 
     # returns the jumper wires state as a string
     def __str__(self):
-        if (self._defused):
+        if (self._locked):
+            return "LOCKED"
+        elif (self._defused):
             return "DEFUSED"
         else:
             # TODO
-            pass
+            return "TODO"
 
 # the pushbutton phase
 class Button(PhaseThread):
@@ -257,6 +351,10 @@ class Button(PhaseThread):
         self._color = color
         # we need to know about the timer (7-segment display) to be able to determine correct pushbutton releases in some cases
         self._timer = timer
+        self._locked = True
+
+    def unlock(self):
+        self._locked = False
 
     # runs the thread
     def run(self):
@@ -266,30 +364,33 @@ class Button(PhaseThread):
         self._rgb[1].value = False if self._color == "G" else True
         self._rgb[2].value = False if self._color == "B" else True
         while (self._running):
-            # get the pushbutton's state
-            self._value = self._component.value
-            # it is pressed
-            if (self._value):
-                # note it
-                self._pressed = True
-            # it is released
-            else:
-                # was it previously pressed?
-                if (self._pressed):
-                    # check the release parameters
-                    # for R, nothing else is needed
-                    # for G or B, a specific digit must be in the timer (sec) when released
-                    if (not self._target or self._target in self._timer._sec):
-                        self._defused = True
-                    else:
-                        self._failed = True
-                    # note that the pushbutton was released
-                    self._pressed = False
+            if (not self._locked):
+                # get the pushbutton's state
+                self._value = self._component.value
+                # it is pressed
+                if (self._value):
+                    # note it
+                    self._pressed = True
+                # it is released
+                else:
+                    # was it previously pressed?
+                    if (self._pressed):
+                        # check the release parameters
+                        # for R, nothing else is needed
+                        # for G or B, a specific digit must be in the timer (sec) when released
+                        if (not self._target or self._target in self._timer._sec):
+                            self._defused = True
+                        else:
+                            self._failed = True
+                        # note that the pushbutton was released
+                        self._pressed = False
             sleep(0.1)
 
     # returns the pushbutton's state as a string
     def __str__(self):
-        if (self._defused):
+        if (self._locked):
+            return "LOCKED"
+        elif (self._defused):
             return "DEFUSED"
         else:
             return str("Pressed" if self._value else "Released")
@@ -298,6 +399,10 @@ class Button(PhaseThread):
 class Toggles(PhaseThread):
     def __init__(self, component, target, name="Toggles"):
         super().__init__(name, component, target)
+        self._locked = True
+
+    def unlock(self):
+        self._locked = False
 
     # runs the thread
     def run(self):
@@ -306,9 +411,10 @@ class Toggles(PhaseThread):
 
     # returns the toggle switches state as a string
     def __str__(self):
-        if (self._defused):
+        if (self._locked):
+            return "LOCKED"
+        elif (self._defused):
             return "DEFUSED"
         else:
             # TODO
-            pass
-
+            return "TODO"
